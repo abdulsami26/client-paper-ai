fbd215294da876f7cecf920d7f587023f7152463
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-10-07 11:57:37 +0500

    implement google login

--- frontend/src/screens/login/login-form.tsx
+++ frontend/src/screens/login/login-form.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import { Button } from "@/components/ui/button"
+import { GoogleLogin } from "@react-oauth/google"
 import { Card, CardContent } from "@/components/ui/card"
 
 export function LoginForm() {
@@ -10,20 +8,15 @@ export function LoginForm() {
         <CardContent className="p-8">
           <form>
             <div className="flex flex-col gap-6">
-              <Button
-                type="button"
-                variant="outline"
-                size="lg"
-                className="w-full gap-3 border-border/50 bg-card text-base font-medium hover:bg-accent hover:text-accent-foreground"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-5">
-                  <path
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                    fill="currentColor"
-                  />
-                </svg>
-                Continue with Google
-              </Button>
+              <GoogleLogin
+                onSuccess={credentialResponse => {
+                  const idToken = credentialResponse.credential;
+                  console.log(idToken)
+                }}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+              />
             </div>
           </form>
         </CardContent>
